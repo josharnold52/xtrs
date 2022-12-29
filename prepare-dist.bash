a29d92689ac157b3e1cb06da948b7458f9c6eb2d
Joshua Arnold <josharnold52@users.noreply.github.com>
2022-12-29 01:36:05 -0500

    Infratructure mods

--- prepare-dist.bash
+++ prepare-dist.bash
@@ -19,6 +19,7 @@ cp cwsdpmi/BIN/CWSDPMI.EXE _dist_gather/ || exit 1
 cp M1L1.ROM _dist_gather/ || exit 1
 cp M1L2.ROM _dist_gather/ || exit 1
 cp ./keytrap/target/KEYTRAP.COM _dist_gather || exit 1
+cp ./launcher/target/LAUNCHER.COM _dist_gather || exit 1
 rsync -a "$SCRIPT_DIR"/../dist-extras/ _dist_gather/ || exit 1
 
 
@@ -30,9 +31,13 @@ echo "
 keytrap.com dosxtrs -model 1 -romfile M1L2.ROM
 " | perl -pe 's/\n/\r\n/g' > _dist_gather/RUN_M1L2.BAT
 
+cat dosbat/LAUNCH.BAT | perl -pe 's/\n/\r\n/g' > _dist_gather/LAUNCH.BAT
+cat dosbat/GETUPD.BAT | perl -pe 's/\n/\r\n/g' > _dist_gather/GETUPD.BAT
+
+
 cd _dist_gather || exit 1
 
-zip "$SCRIPT_DIR"/../dist/dosxtrs.zip *
+zip -r "$SCRIPT_DIR"/../dist/dosxtrs.zip *
 
 cd "$SCRIPT_DIR"
 
--- serve-dist.bash
+++ serve-dist.bash
@@ -8,6 +8,6 @@ mkdir -p "$SCRIPT_DIR"/../dist
 cd "$SCRIPT_DIR"/../dist
 
 ifconfig  | grep -E 'inet\s'
-
+ls
 python3 -m http.server 7777
 
